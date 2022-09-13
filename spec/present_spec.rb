9dd7307443e8a208840f41df93fa0e3afb27ac40
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2022-09-13 20:36:39 +0100

    Present exercise completed

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe Present do
     it "fails" do
       item = Present.new
       item.wrap('Playstation')
-      expect { item.wrap('xbox') }.to raise_error "A contents has already been wrapped"
+      expect { item.wrap('xbox') }.to raise_error "A contents has already been wrapped."
+    end
+  context "when a present is unwrapped"
+    it "fails" do
+      item = Present.new
+      expect { item.unwrap() }.to raise_error "No contents have been wrapped."
     end
   end
 end
